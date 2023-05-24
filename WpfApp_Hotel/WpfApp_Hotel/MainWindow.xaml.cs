d316b61ba1ec8ca2c394786d072cebcbb7cd6892
unknown <gosgie02@wp.pl>
2023-05-24 17:46:31 +0200

    dodano i przetestowano połączenie z bazą danych

---
+++ WpfApp_Hotel/WpfApp_Hotel/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Hotel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = "data source=localhost;initial catalog=hotel;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Połączono z bazą danych");
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show("Nie udało się połączyć z bazą danych");
+                }
+            }
+        }
+    }
+}
