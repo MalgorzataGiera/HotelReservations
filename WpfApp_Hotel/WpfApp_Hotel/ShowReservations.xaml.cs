f84dc4ea5870a3a0fc8a6c82ea2392de7cc93565
unknown <gosgie02@wp.pl>
2023-05-25 03:42:00 +0200

    po wybraniu rezerwacji wyświetlają się jej szczegóły

--- WpfApp_Hotel/WpfApp_Hotel/ShowReservations.xaml.cs
+++ WpfApp_Hotel/WpfApp_Hotel/ShowReservations.xaml.cs
@@ -36,7 +36,7 @@ namespace WpfApp_Hotel
                 {
                     connection.Open();
                     // Zapytanie SQL do pobrania wszystkich rezerwacji
-                    string query = $"SELECT g.LastName, g.FirstName, r.CheckInDate, r.CheckOutDate FROM Guests g JOIN Reservations r ON g.GuestId = r.GuestID";
+                    string query = $"SELECT g.LastName, g.FirstName, r.CheckInDate, r.CheckOutDate, r.ReservationID FROM Guests g JOIN Reservations r ON g.GuestId = r.GuestID";
 
                     SqlCommand command = new SqlCommand(query, connection);
                     SqlDataReader reader = command.ExecuteReader();
@@ -48,13 +48,14 @@ namespace WpfApp_Hotel
                         string FirstName = reader["FirstName"].ToString();
                         DateTime checkInDate = (DateTime)reader["CheckInDate"];
                         DateTime checkOutDate = (DateTime)reader["CheckOutDate"];
+                        string resID = reader["ReservationID"].ToString();
 
                         // Zmiana formatu daty
                         string formattedCheckInDate = checkInDate.ToString("yyyy.MM.dd");
                         string formattedCheckOutDate = checkOutDate.ToString("yyyy.MM.dd");
 
 
-                        reservationsListBox.Items.Add(LastName + " " + FirstName + " " + formattedCheckInDate + " - " + formattedCheckOutDate);
+                        reservationsListBox.Items.Add(resID + " " + LastName + " " + FirstName + " " + formattedCheckInDate + " - " + formattedCheckOutDate);
                     }
                     reader.Close();
                 }
@@ -73,7 +74,7 @@ namespace WpfApp_Hotel
                 {
                     connection.Open();
                     // Zapytanie SQL do pobrania wszystkich rezerwacji
-                    string query = $"SELECT g.LastName, g.FirstName, r.CheckInDate, r.CheckOutDate FROM Guests g JOIN Reservations r ON g.GuestId = r.GuestID ORDER BY {option}";
+                    string query = $"SELECT g.LastName, g.FirstName, r.CheckInDate, r.CheckOutDate, r.ReservationID FROM Guests g JOIN Reservations r ON g.GuestId = r.GuestID ORDER BY {option}";
 
                     SqlCommand command = new SqlCommand(query, connection);
                     command.Parameters.AddWithValue("@SortOption", option); // Przekazanie wartości wybranej opcji jako parametru
@@ -85,17 +86,19 @@ namespace WpfApp_Hotel
                     // Przypisanie wyników do kontrolki ListBox
                     while (reader.Read())
                     {
+                        string resID = reader["ReservationID"].ToString();
                         string LastName = reader["LastName"].ToString();
                         string FirstName = reader["FirstName"].ToString();
+                        //string resID = reader["ReservationID"].ToString();
                         DateTime checkInDate = (DateTime)reader["CheckInDate"];
                         DateTime checkOutDate = (DateTime)reader["CheckOutDate"];
 
                         // Zmiana formatu daty
                         string formattedCheckInDate = checkInDate.ToString("yyyy.MM.dd");
                         string formattedCheckOutDate = checkOutDate.ToString("yyyy.MM.dd");
-
                         
-                        reservationsListBox.Items.Add(LastName + " " + FirstName + " " + formattedCheckInDate + " - " + formattedCheckOutDate);
+                        
+                        reservationsListBox.Items.Add(resID + " " + LastName + " " + FirstName + " " + formattedCheckInDate + " - " + formattedCheckOutDate);
                     }
                     reader.Close();
                 }
@@ -118,10 +121,76 @@ namespace WpfApp_Hotel
                 case "5": option = "CheckInDate DESC"; break;
                 case "6": option = "CheckOutDate ASC"; break;
                 case "7": option = "CheckOutDate DESC"; break;
-
+                default: option = "ReservationID ASC"; break;
             }
-            
             UpdateReservationsList();
+        }
+        //private void reservationsListBox_Click(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (reservationsListBox.SelectedItem != null)
+        //    {
+        //        // Pobierz wybrany element ListBox
+        //        string selectedReservation = reservationsListBox.SelectedItem.ToString();
+
+        //        // Wykonaj określone działania dla wybranego elementu
+        //        // np. wyświetl szczegóły rezerwacji w innym kontrolce
+        //        MessageBox.Show("info");
+        //    }
+        //}
+
+        private void reservationsListBox_Click(object sender, MouseButtonEventArgs e)
+        {
+            string oneID ="";
+            if (reservationsListBox.SelectedItem != null)
+            {
+                //Pobierz wybrany element ListBox
+                string selectedReservation = reservationsListBox.SelectedItem.ToString();
+
+                //Podziel wybrany element na poszczególne części
+                string[] parts = selectedReservation.Split(' ');
+
+                if (parts.Length >= 2)
+                    oneID = parts[0];
+            }
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    // Zapytanie SQL do pobrania wszystkich informacji o rezerwacji
+                    string query = $"SELECT r.*, g.FirstName AS gFirstName, g.LastName AS gLastName, e.FirstName AS eFirstName, e.LastName AS eLastName FROM Reservations r JOIN Guests g ON r.GuestID = g.GuestID JOIN Employees e ON r.EmployeeID = e.EmployeeID WHERE r.ReservationID = {oneID}";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@SortOption", oneID);
+                    SqlDataReader reader = command.ExecuteReader();
+                   
+                    // Wypisanie wyników 
+                    while (reader.Read())
+                    {
+                        string gLastName = reader["gLastName"].ToString();
+                        string gFirstName = reader["gFirstName"].ToString();
+                        DateTime checkInDate = (DateTime)reader["CheckInDate"];
+                        DateTime checkOutDate = (DateTime)reader["CheckOutDate"];
+                        string room = reader["RoomNumber"].ToString();
+                        string status = reader["Status"].ToString();
+                        string eLastName = reader["eLastName"].ToString();
+                        string eFirstName = reader["eFirstName"].ToString();
+                        string ifSettled = reader["IfSettled"].ToString();
+                        string GuestID = reader["GuestID"].ToString();
+
+                        // Zmiana formatu daty
+                        string formattedCheckInDate = checkInDate.ToString("yyyy.MM.dd");
+                        string formattedCheckOutDate = checkOutDate.ToString("yyyy.MM.dd");
+
+                        MessageBox.Show($"Guest: {gFirstName} {gLastName} | Reservation from: {formattedCheckInDate} to: {formattedCheckOutDate} | Room nr: {room} | Status: {status} | Settled: {ifSettled} | Reservations accepted by: {eFirstName} {eLastName}");
+                    }
+                    reader.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Wystąpił błąd podczas pobierania szczegółowych informacji o rezerwacji: " + ex.Message, "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
